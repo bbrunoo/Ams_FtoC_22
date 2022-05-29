9604e23e7f4a37e1b9556cc26583de9ed22d5eb3
bbrunoo <brunoedsanntos@gmail.com>
2022-05-29 13:31:46 -0300

    atv05

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double Graus = 0.0;
+double Fahrenheit = 0.0;
+
+Console.WriteLine("Escreva a temperatura em Fahrenheit para a conversao para Graus:");
+Fahrenheit = Convert.ToInt32(Console.ReadLine());
+
+Graus = (Fahrenheit - 32) * 5 / 9;
+
+Console.WriteLine($"{Fahrenheit} Fahrenheit em Graus é igual á: {Graus}");
